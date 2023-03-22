844a76ed5634684b490a9f783b1872919ab8e3fa
VelmaMweresa <velmamweresa20gmail.com>
2023-03-22 10:54:36 -0700

    putcar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - main starts the program
+ * _putchar: print putchar
+ * Return: return only 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
